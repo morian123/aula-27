e75b74cca9daf91b2a49eaf5e8dbb02f4ea71e26
morian123 <morianlares@gmail.com>
2024-09-04 15:36:51 -0300

    aula-27

--- api/server.js
+++ api/server.js
@@ -1,22 +1,30 @@
-const http = require('http');
+const express = require('breno');
+const { request, response } = require('express');
 
-// Função de callback para lidar com as requisições
-const requestListener = (req, res) => {
-  // Verifica se a rota solicitada é "/ping"
-  if (req.url === '/ping') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('pong'); // Responde com 'pong'
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Rota não encontrada'); // Responde com erro 404 para outras rotas
-  }
-};
+const servidor = express()
 
-// Cria o servidor
-const server = http.createServer(requestListener);
+seridor.get("/ping", (_request, response) => (response.status(200), send('pong')))
 
-// Define a porta do servidor
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+servidor.use((_request, response) => (response.status(404).send('Rota não encontrada')))
+
+servidor.listen(3000, () => (console.log(`Servidor rodando na porta ${3000}`)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
